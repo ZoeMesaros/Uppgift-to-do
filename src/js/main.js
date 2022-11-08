afd381b4a632f68efff44bf5b4ca042328b8bedb
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2022-11-08 21:14:20 +0100

    New

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+let toDo = ["Gå ut med hunden", "Laga mat", "Vattna blommorna", "Tvätta"]
+let listitem = document.getElementById('myList')
+
+function handletodo (e) {
+e.target.style.textDecoration = "line-through"
+}
+
+for (let i = 0; i < toDo.length; i++) {
+let item = document.createElement("li");
+item.addEventListener("click", handletodo);
+item.appendChild(document.createTextNode(toDo[i]))
+listitem.appendChild(item);
+}
